7c43e39079bf3cff6c720e26106b23d1c015938e
Danilo Sampaio <daniloarpesam@gmail.com>
2020-04-29 01:25:05 -0300

    Updates

---
+++ precache-manifest.b5ff2ffb65068148b2ddb53d37330546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514c8dd3e60435fa46ba71814af7ebfc",
+    "url": "/react-get-hired/index.html"
+  },
+  {
+    "revision": "d52e9ca011b2d8517208",
+    "url": "/react-get-hired/static/css/main.52283547.chunk.css"
+  },
+  {
+    "revision": "9a5256d2706dc5f00aa3",
+    "url": "/react-get-hired/static/js/2.ddb67eca.chunk.js"
+  },
+  {
+    "revision": "d00098666f3a9b89d824bdeb15ece8eb",
+    "url": "/react-get-hired/static/js/2.ddb67eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52e9ca011b2d8517208",
+    "url": "/react-get-hired/static/js/main.648432ba.chunk.js"
+  },
+  {
+    "revision": "26baf2e50207be3d3c00",
+    "url": "/react-get-hired/static/js/runtime-main.c2cdc2d7.js"
+  }
+]);
